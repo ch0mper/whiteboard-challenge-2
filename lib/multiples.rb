c728ff1c562920967f477e4787c7d9baf7812850
ch0mper <hannahchang17@gmail.com>
2018-10-23 10:58:01 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    array = (1..limit-1).to_a
+    new_array = array.select do |num|
+        num % 3 == 0 || num % 5 == 0
+    end
+end
+
+def sum_multiples(limit)
+    new_array = collect_multiples(limit)
+    new_array.inject(0){|sum,x| sum + x }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+require_relative './multiples.rb'
+
+class Multiples
+
+    attr_accessor :limit, :array
+
+    def initialize(limit)
+        @limit = limit
+        @array = (1..limit-1).to_a
+    end
+
+    def collect_multiples
+        new_array = @array.select do |num|
+            num % 3 == 0 || num % 5 == 0
+        end
+    end
+
+    def sum_multiples
+        new_array = collect_multiples
+        new_array.inject(0){|sum,x| sum + x }
+    end
+
+end
